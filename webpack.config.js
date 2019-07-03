d16db72d12fb5b98abbde2d09b161116286e9d25
Helen Yu <helenyueyu@gmail.com>
2019-07-03 07:29:17 -0700

    Finish webpack.config.js file and edited the output destination to be './app/assets/javascripts/bundle.js'. Also added 'start' script to package .json to automatically run watch

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+module.exports = {
+    entry: "./frontend/bug_mojo.jsx",
+    output: {
+        path: path.resolve(__dirname),
+        filename: "./app/assets/javascripts/bundle.js"
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+}
