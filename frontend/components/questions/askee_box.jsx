766d6d8ac457312c7e6ab2f8f5e7d139b03c52e6
tokyosuite <helenyueyu@gmail.com>
2020-05-10 19:51:09 -0700

    change .css to .scss

--- frontend/components/questions/askee_box.jsx
+++ frontend/components/questions/askee_box.jsx
@@ -12,39 +12,26 @@ class AskeeBox extends React.Component {
             return (
                 <div className="askee_box">
                     <div className="askee_box_time">asked {moment(this.props.question.createdAt).fromNow()}</div>
-                    <div style={{ display: 'flex', flexDirection: 'row' }}>
-                        <img style={{ marginRight: '0.2rem', marginTop: '0.2rem', marginRight: '0.5rem' }} width="35px" height="35px" src={this.props.image} alt="identicon" />
-                        <div style={{ display: 'flex', flexDirection: 'column' }}>
-                            <div style={{ color: '#0076cb', fontSize: '0.9rem' }}>USERNAME GOES HERE</div>
-                            <div style={{ display: 'flex', flexDirection: 'row', transform: 'translateY(-3px)', fontSize: '0.8rem' }}>
+                    <div className="askee_box_identicon_username_flex">
+                        <img className="askee_box_identicon" src={this.props.image} alt="identicon" />
+                        <div className="askee_box_username_medals_flex">
+                            <div className="askee_username">USERNAME</div>
+                            <div className="medal_row_list">
 
-                                <div style={{ color: 'gray', fontWeight: 'bold' }}>{Math.floor(Math.random() * 10)}</div>
-
+                                <div className="reputation">{15}</div>
                                 <>
-                                    <div style={{ marginLeft: '0.45rem', marginRight: '0.2rem', transform: 'translateY(-6px)', fontSize: '1.3rem', color: '#ffcf0f', opacity: 0.8 }}>
-                                        <span>&#8226;</span>
-                                    </div>
+                                    <div className="gold_badge"><span>&#8226;</span></div>
                                     <div style={{ color: 'gray' }}>{34}</div>
                                 </>
-
-
                                 <>
-                                    <div style={{ marginLeft: '0.45rem', marginRight: '0.2rem', transform: 'translateY(-6px)', fontSize: '1.3rem', color: '#A8A8A8', opacity: 0.8 }}>
-                                        <span>&#8226;</span>
-                                    </div>
+                                    <div className="silver_badge"><span>&#8226;</span></div>
                                     <div style={{ color: 'gray' }}>{33}</div>
                                 </>
 
-
-
                                 <>
-                                    <div style={{ marginLeft: '0.45rem', marginRight: '0.2rem', transform: 'translateY(-6px)', fontSize: '1.3rem', color: '#965A38', opacity: 0.8 }}>
-                                        <span>&#8226;</span>
-                                    </div>
+                                    <div className="bronze_badge"><span>&#8226;</span></div>
                                     <div style={{ color: 'gray' }}>{35}</div>
                                 </>
-
-
                             </div>
                         </div>
                     </div>
